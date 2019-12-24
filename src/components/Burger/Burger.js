6aaa4f92fee9696f3086f5c585a92895db59752f
felipe de alba <felipe.de.alba.alegre@gmail.com>
2019-12-24 18:05:22 +0100

    asd

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,24 @@ import React from 'react';
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = props => {
+const burger = ({ ingredients }) => {
+
+  let transformedIngredients = Object.keys(ingredients)
+    .map(igKey => {
+      return [...Array(ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      })
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients</p>
+  }
   return (
       <div className={classes.Burger}>
         <BurgerIngredient type="bread-top"/>
-        <BurgerIngredient type="cheese"/>
-        <BurgerIngredient type="meat"/>
+        {transformedIngredients}
         <BurgerIngredient type="bread-bottom"/>
       </div>
     );
